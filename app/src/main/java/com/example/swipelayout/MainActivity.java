7c789ca4d148e199606f19fa18bc865a9761f723
omeriyioz <omer.iyioz@consultant.turkcell.com.tr>
2020-01-16 11:37:24 +0300

    swipeLayout example using the library of https://github.com/daimajia/AndroidSwipeLayout      implementation "com.daimajia.swipelayout:library:1.2.0@aar"

---
+++ app/src/main/java/com/example/swipelayout/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.swipelayout;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.daimajia.swipe.util.Attributes;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * RecyclerView: The new recycler view replaces the list view. Its more modular and therefore we
+     * must implement some of the functionality ourselves and attach it to our recyclerview.
+     * <p/>
+     * 1) Position items on the screen: This is done with LayoutManagers
+     * 2) Animate & Decorate views: This is done with ItemAnimators & ItemDecorators
+     * 3) Handle any touch events apart from scrolling: This is now done in our adapter's ViewHolder
+     */
+
+    private ArrayList<Student> mDataSet;
+
+    private Toolbar toolbar;
+
+    private TextView tvEmptyView;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.swiprecyclerview);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvEmptyView = (TextView) findViewById(R.id.empty_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // Layout Managers:
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Item Decorator:
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getResources().getDrawable(R.drawable.divider)));
+        // mRecyclerView.setItemAnimator(new FadeInLeftAnimator());
+
+
+        mDataSet = new ArrayList<Student>();
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle("Android Students");
+
+        }
+
+        loadData();
+
+        if (mDataSet.isEmpty()) {
+            mRecyclerView.setVisibility(View.GONE);
+            tvEmptyView.setVisibility(View.VISIBLE);
+
+        } else {
+            mRecyclerView.setVisibility(View.VISIBLE);
+            tvEmptyView.setVisibility(View.GONE);
+        }
+
+
+        // Creating Adapter object
+        SwipeRecyclerViewAdapter mAdapter = new SwipeRecyclerViewAdapter(this, mDataSet);
+
+
+        // Setting Mode to Single to reveal bottom View for one item in List
+        // Setting Mode to Mutliple to reveal bottom Views for multile items in List
+        ((SwipeRecyclerViewAdapter) mAdapter).setMode(Attributes.Mode.Single);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        /* Scroll Listeners */
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.e("RecyclerView", "onScrollStateChanged");
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
+
+
+    // load initial data
+    public void loadData() {
+
+        for (int i = 0; i <= 20; i++) {
+            mDataSet.add(new Student("Student " + i, "androidstudent" + i + "@gmail.com"));
+
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/swipelayout/SwipeRecyclerViewAdapter.java
@@ -0,0 +1,186 @@
+package com.example.swipelayout;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+
+import java.util.ArrayList;
+
+public class SwipeRecyclerViewAdapter extends RecyclerSwipeAdapter<SwipeRecyclerViewAdapter.SimpleViewHolder> {
+
+
+    private Context mContext;
+    private ArrayList<Student> studentList;
+
+    public SwipeRecyclerViewAdapter(Context context, ArrayList<Student> objects) {
+        this.mContext = context;
+        this.studentList = objects;
+    }
+
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_row_item, parent, false);
+        return new SimpleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final SimpleViewHolder viewHolder, final int position) {
+        final Student item = studentList.get(position);
+
+        viewHolder.tvName.setText((item.getName()) + "  -  Row Position " + position);
+        viewHolder.tvEmailId.setText(item.getEmailId());
+
+
+        viewHolder.swipeLayout.setShowMode(SwipeLayout.ShowMode.PullOut);
+
+        // Drag From Left
+        viewHolder.swipeLayout.addDrag(SwipeLayout.DragEdge.Left, viewHolder.swipeLayout.findViewById(R.id.bottom_wrapper1));
+
+        // Drag From Right
+        viewHolder.swipeLayout.addDrag(SwipeLayout.DragEdge.Right, viewHolder.swipeLayout.findViewById(R.id.bottom_wrapper));
+
+
+        // Handling different events when swiping
+        viewHolder.swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
+
+        /*viewHolder.swipeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if ((((SwipeLayout) v).getOpenStatus() == SwipeLayout.Status.Close)) {
+                    //Start your activity
+
+                    Toast.makeText(mContext, " onClick : " + item.getName() + " \n" + item.getEmailId(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });*/
+
+        viewHolder.swipeLayout.getSurfaceView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, " onClick : " + item.getName() + " \n" + item.getEmailId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        viewHolder.btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(v.getContext(), "Clicked on Map " + viewHolder.tvName.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        viewHolder.tvShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(view.getContext(), "Clicked on Share " + viewHolder.tvName.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewHolder.tvEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(view.getContext(), "Clicked on Edit  " + viewHolder.tvName.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        viewHolder.tvDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mItemManger.removeShownLayouts(viewHolder.swipeLayout);
+                studentList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, studentList.size());
+                mItemManger.closeAllItems();
+                Toast.makeText(view.getContext(), "Deleted " + viewHolder.tvName.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // mItemManger is member in RecyclerSwipeAdapter Class
+        mItemManger.bindView(viewHolder.itemView, position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe;
+    }
+
+
+    //  ViewHolder Class
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
+        SwipeLayout swipeLayout;
+        TextView tvName;
+        TextView tvEmailId;
+        TextView tvDelete;
+        TextView tvEdit;
+        TextView tvShare;
+        ImageButton btnLocation;
+
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+            swipeLayout = (SwipeLayout) itemView.findViewById(R.id.swipe);
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            tvEmailId = (TextView) itemView.findViewById(R.id.tvEmailId);
+            tvDelete = (TextView) itemView.findViewById(R.id.tvDelete);
+            tvEdit = (TextView) itemView.findViewById(R.id.tvEdit);
+            tvShare = (TextView) itemView.findViewById(R.id.tvShare);
+            btnLocation = (ImageButton) itemView.findViewById(R.id.btnLocation);
+
+
+        }
+    }
+}
